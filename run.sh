c0a7ab0a56fa6eb18783d1ce1bf36025993a179f
Qrust <Qrust@users.noreply.github.com>
2014-11-26 12:06:51 +0300

    Добавлена автогенерация файлов python и их запуск

--- run.sh
+++ run.sh
@@ -3,12 +3,29 @@
 clear && clear
 
 FILE="Strong_vigenere_cipher-bin"
-PASSWORD="k1z2u3"
 
+LEN=$1
+TESTS=$2
 
 BASEDIR="$PWD"
 
 make -C "$BASEDIR" rebuild
 
-echo ""
-./$FILE -p $PASSWORD
+echo "Начинаем генерацию тестов"
+
+for ticks in `seq 1 $TESTS`
+do
+	PASSWORD=`cat /dev/urandom | tr -dc "a-zA-Z0-9@#*=[]" | dd  bs=$LEN count=1 2>/dev/null`
+	echo "Шифруем на $PASSWORD"
+	./$FILE -p $PASSWORD
+done
+
+cd ./py_tests
+
+FILES="./*.py"
+
+for files in $FILES
+do
+	echo "Строим график $files"
+	python3 $files
+done
