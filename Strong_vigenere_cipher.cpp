7a0696f61b08e198c6a9aa6f66a4d378d1d55bbb
Qrust <Qrust@users.noreply.github.com>
2014-12-07 16:13:59 +0300

    В это момент забил на стандартный алфавит. Создам свой, и буду сравнивать, печаль-беда

--- Strong_vigenere_cipher.cpp
+++ Strong_vigenere_cipher.cpp
@@ -12,67 +12,95 @@
 #include <fstream>
 #include <vector>
 #include <unistd.h>
+#include <stdlib.h>
+#include <algorithm>    // std::reverse
 
 #include "hash.h"
 
-//#define DEBUG
+#define DEBUG
 //#define VIGENERE
 
 using namespace std;
 
-void MakePythonFile (HashTable *plane, HashTable *encr, string password);
-void Revision_1 (string password, vector<unsigned> passOffset, char *buffer, unsigned long long int size);
+// Делает питоновский файл в исходной директории для построения графика
+void MakePythonFile(HashTable *plane, HashTable *encr, string password, int type);
+
+// Простой виженер
+void Revision_0(string password, vector<unsigned> passOffset, char *buffer, unsigned long long int size);
+
+// Шифрование с уменьшением длины пароля
+void Revision_1(string password, vector<unsigned> passOffset, char *buffer, unsigned long long int size);
+
+// Шифрование с уменьшением длины пароля, НО ШИФРУЮТСЯ ТОЛЬКО БУКВЫ (БЕЗ РЕГИСТРА)
+void Revision_1_1(string password, vector<unsigned> passOffset, char *buffer, unsigned long long int size);
+
+// Шифрование с уменьшением длины пароля и инвертированем
+void Revision_2(string password, vector<char> alphabet, char *buffer, unsigned long long int size);
 
 int main(int argc, char **argv)
 {
-
 /*============================================================================
-|	Парсинг пароля
+|	Парсинг -пароль -метод_шифрования 
 *============================================================================*/
 	string password;
+	int method = 0;
 	
 	int opt;
-	while ((opt = getopt(argc, argv, "p:") ) != -1) 
+	while ((opt = getopt(argc, argv, "p:m:") ) != -1) 
 	{
 		switch (opt) 
 		{
 			case 'p':
 				password = optarg;
 				break;
+			case 'm':
+				method = atoi(optarg);
+				break;				
 			default:
-				cout << "Использовать вот так: %s -p [password]\n" << argv[0] << endl;
+				cout << "Использовать вот так: %s -p [password] -m [method]\n" << argv[0] << endl;
 				return 0;
 		}
 	}
 	
 	#ifdef DEBUG
-	cout << "ПАРОЛЬ: " <<  password << endl
-		 << "Длина пароля: " << password.size() << endl
-		 << endl;
+		cout << "ПАРОЛЬ: " <<  password << endl
+			 << "Длина пароля: " << password.size() << endl
+			 << endl;
 	#endif
-
 /*============================================================================
 |	Заполняем алфавит
 *============================================================================*/
-	vector<char> alphabet(126 - 32 + 1);
-	
-	for (unsigned i = 0; i < alphabet.size(); i++)
-		alphabet[i] = (char)(i+32);
+	int alphabet_len = (26+1);
+	vector<char> alphabet(alphabet_len);
+
+	switch (alphabet_len) 
+	{
+		case (26+1):
+			for (unsigned i = 0; i < alphabet.size(); i++)
+				alphabet[i] = (char)(i+97);
+			break;		
+		case (126-32+1):
+			for (unsigned i = 0; i < alphabet.size(); i++)
+				alphabet[i] = (char)(i+32);
+			break;		
+		default:
+			cout << "Определите размер алфавита" << endl;
+			return 0;
+	}
 
 	#ifdef DEBUG
-	cout << "АЛФАВИТ:" << endl;
+		cout << "АЛФАВИТ:" << endl;
 
-	for (unsigned i = 0; i < alphabet.size(); i++)
-		//cout << "[" << alphabet[i] << "] ";
-		cout << alphabet[i] << "";
-		//cout <<"["<< i <<"]"<< "[" << alphabet[i] << "]" << endl;
-	cout << endl;
+		for (unsigned i = 0; i < alphabet.size(); i++)
+			//cout << "[" << alphabet[i] << "] ";
+			cout << alphabet[i] << "";
+			//cout <<"["<< i <<"]"<< "[" << alphabet[i] << "]" << endl;
+		cout << endl;
 
-	cout << endl
-		 << "Размер алфавита: " << alphabet.size() << endl
-		 << endl;
+		cout << endl
+			 << "РАЗМЕР АЛФАВИТА: " << alphabet.size() << endl
+			 << endl;
 	#endif
-
 /*============================================================================
 |	Считаем смещение каждой буквы пароля
 *============================================================================*/
@@ -84,42 +112,40 @@ int main(int argc, char **argv)
 		{
 			if (password[i] == alphabet[k])
 			{
-				passOffset[i] = (unsigned)alphabet[k] - 32;
+				passOffset[i] = (unsigned)alphabet[k] - (unsigned)alphabet[0];
 				break;
 			}
 		}
 	}
 
 	#ifdef DEBUG
-	cout << "Сдвиг у пароля: "<< endl;
-	for (unsigned i = 0; i < passOffset.size(); i++)
-	{
-		cout <<"["<< i <<"]"<< "[" << passOffset[i] << "]" << endl;
-	}
+		cout << "Сдвиг у пароля: "<< endl;
+		for (unsigned i = 0; i < passOffset.size(); i++)
+		{
+			cout <<"["<< i <<"]"<< "[" << passOffset[i] << "]" << endl;
+		}
 		cout << endl;
 	#endif
-
 /*============================================================================
 |	Моя таблица Виженера
 *============================================================================*/
 	#ifdef DEBUG
 		#ifdef VIGENERE
-		cout << "Моя таблица Виженера:" << endl;
-		for (unsigned i = 0; i < alphabet.size(); i++)
-		{
-			for (unsigned k = i; k < alphabet.size(); k++)
-				cout << alphabet[k] << "";
+			cout << "Моя таблица Виженера:" << endl;
+			for (unsigned i = 0; i < alphabet.size(); i++)
+			{
+				for (unsigned k = i; k < alphabet.size(); k++)
+					cout << alphabet[k] << "";
 
-			for (unsigned k = 0; k < i; k++)
-				cout << alphabet[k] << "";
-			
-			cout << endl;
-			//cout << endl << endl;
-		}
-			cout << endl;
+				for (unsigned k = 0; k < i; k++)
+					cout << alphabet[k] << "";
+				
+				cout << endl;
+				//cout << endl << endl;
+			}
+				cout << endl;
 		#endif
 	#endif
-
 /*============================================================================
 |	Файлы для чтения записи
 *============================================================================*/
@@ -131,7 +157,6 @@ int main(int argc, char **argv)
 		cout << "Файл text.txt не может быть открыт!\n";
 		return 0;
 	}
-
 /*============================================================================
 |	Пишем в буфер для последующей обработки
 *============================================================================*/
@@ -146,14 +171,13 @@ int main(int argc, char **argv)
 	char *buffer = new char [size];
 	plainText.read(buffer, size);
 
-/*	#ifdef DEBUG
+	/*#ifdef DEBUG
 	cout << "/==========================================================" << endl
 		 << buffer << endl
 		 << "\\----------------------------------------------------------" << endl 
 		 << endl
 		 << endl;
 	#endif*/
-
 /*============================================================================
 |	Частотный анализ planeText
 *============================================================================*/
@@ -166,20 +190,32 @@ int main(int argc, char **argv)
 	#ifdef DEBUG
 		planeTable.PrintChainsIf();
 	#endif
-
 /*============================================================================
-|	Кодируем ключем с раундами равными длине ключа
+|	Кодируем ключем в зависимости от медота
 *============================================================================*/
-/*	#ifdef DEBUG
+	/*
+	#ifdef DEBUG
 	cout << "/----------------------------------------------------------" << endl
 		 << buffer << endl
 		 << "\\----------------------------------------------------------" << endl 
 		 << endl;
 	#endif
-*/
-
-	Revision_1 (password, passOffset, buffer, size);
-
+	*/
+	switch (method) 
+	{
+		case 0:
+			Revision_0 (password, passOffset, buffer, size);
+			break;		
+		case 1:
+			Revision_1 (password, passOffset, buffer, size);
+			break;
+		case 2:
+			Revision_2 (password, alphabet, buffer, size);
+			break;			
+		default:
+			Revision_0 (password, passOffset, buffer, size);
+			break;
+	}
 /*============================================================================
 |	Частотный анализ encryptedText
 *============================================================================*/
@@ -192,17 +228,16 @@ int main(int argc, char **argv)
 	#ifdef DEBUG
 		encryptedTable.PrintChainsIf();
 	#endif
-
 /*============================================================================
 |	Пишем питонский файлик для графопостроителя
 *============================================================================*/
-	MakePythonFile (&planeTable, &encryptedTable, password);
-
+	// ВНИМАНИЕ, ЗНАЧЕНИЕ ПОСЛЕДНЕЙ ЦИФРЫ СМОТРИ ВНУТРИ ФУНКЦИИ
+	MakePythonFile (&planeTable, &encryptedTable, password, 2);
 /*============================================================================
 |	Запись шифротекста в файл
 *============================================================================*/
-
-/*	plainText.seekg(0, plainText.beg);
+	/*	
+	plainText.seekg(0, plainText.beg);
 	while( plainText.get(c) )
 	{	
 		cout << "[" << c << "]" << "_" << plainText.tellg() << " "  ;
@@ -210,8 +245,9 @@ int main(int argc, char **argv)
 	}
 	*/
 	//encryptedText.close();
-
-//===========================================================================//
+/*============================================================================
+|	Очистка и выход
+*============================================================================*/
 	#ifdef DEBUG
 	cout << endl << "Выход из программы" << endl;
 	#endif
@@ -221,16 +257,16 @@ int main(int argc, char **argv)
 	return 0;
 }
 
-void MakePythonFile (HashTable *plane, HashTable *encr, string password)
+void MakePythonFile (HashTable *plane, HashTable *encr, string password, int type)
 {
 	string nameFile; 
-	nameFile = "./py_tests/plotdata_" + password;
+	nameFile = "./plotdata_" + password;
 	nameFile += ".py";
 	ofstream pyCode(nameFile.c_str());
 	
 	if ( !pyCode.is_open())
 	{
-		cout << nameFile << " не может быть открыт!\n";
+		cout << nameFile << " Питоновский файл не может быть открыт!\n";
 	}
 
 	pyCode <<
@@ -238,14 +274,44 @@ void MakePythonFile (HashTable *plane, HashTable *encr, string password)
 	"" << endl <<
 	"import numpy as np" << endl <<
 	"import matplotlib.pyplot as plt" << endl <<
-	"" << endl <<
-	"N = 95" << endl <<
-	"" << endl <<
+	"" << endl;
+	
+	switch (type) 
+	{
+		case 1:
+			pyCode << "N = 94" << endl;
+			break;
+		case 2:
+			pyCode << "N = 95" << endl;
+			break;
+		case 3:
+			pyCode << "N = 26" << endl;// Смотрим только буквы
+			break;
+		default:
+			pyCode << "N = 95" << endl;
+			break;
+	}
+
+	pyCode << "" << endl <<
 	"PlainText = (";
 	
 	streambuf *coutbuf = cout.rdbuf();
 	cout.rdbuf(pyCode.rdbuf());
-	plane->PrintChainsForPython();
+	switch (type) 
+	{
+		case 1:
+			plane->PrintChainsForPythonNoSpace();
+			break;
+		case 2:
+			plane->PrintChainsForPython();
+			break;
+		case 3:
+			plane->PrintChainsForPython();
+			break;
+		default:
+			plane->PrintChainsForPython();
+			break;
+	}
 	cout.rdbuf(coutbuf);
 
 	pyCode <<
@@ -255,7 +321,21 @@ void MakePythonFile (HashTable *plane, HashTable *encr, string password)
 
 	coutbuf = cout.rdbuf();
 	cout.rdbuf(pyCode.rdbuf());
-	encr->PrintChainsForPython();
+	switch (type) 
+	{
+		case 1:
+			encr->PrintChainsForPythonNoSpace();
+			break;
+		case 2:
+			encr->PrintChainsForPython();
+			break;
+		case 3:
+			encr->PrintChainsForPython();
+			break;			
+		default:
+			encr->PrintChainsForPython();
+			break;
+	}
 	cout.rdbuf(coutbuf);
 
 	pyCode <<
@@ -269,8 +349,25 @@ void MakePythonFile (HashTable *plane, HashTable *encr, string password)
 	"ax.set_title('Частотный анализ')" << endl <<
 	"ax.set_ylabel('Количество символов')" << endl <<
 	"ax.set_xlabel('Алфавит')" << endl <<
-	"ax.set_xticks(ind+width)" << endl <<
-	"ax.set_xticklabels( (' ', '!', '\"', '#', '$', '%', '&', '\\\'', '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\\\', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~') )" << endl <<
+	"ax.set_xticks(ind+width)" << endl;
+
+	switch (type) 
+	{
+		case 1:
+			pyCode << "ax.set_xticklabels( ('!', '\"', '#', '$', '%', '&', '\\\'', '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\\\', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~') )" << endl;
+			break;
+		case 2:
+			pyCode << "ax.set_xticklabels( (' ', '!', '\"', '#', '$', '%', '&', '\\\'', '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\\\', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~') )" << endl;
+			break;
+		case 3:
+			pyCode << "ax.set_xticklabels( ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z') )" << endl;
+			break;
+		default:
+			pyCode << "ax.set_xticklabels( (' ', '!', '\"', '#', '$', '%', '&', '\\\'', '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\\\', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~') )" << endl;
+			break;
+	}
+	
+	pyCode << 
 	"ax.legend( (rects1[0], rects2[0]), ('PlainText', 'CryptedText') )" << endl <<
 	"ax.set_xlim(0,N-width/2)" << endl <<
 	"fig.set_size_inches(24,15)" << endl <<
@@ -279,6 +376,37 @@ void MakePythonFile (HashTable *plane, HashTable *encr, string password)
 	".png', format = 'png', bbox_inches='tight')" << endl;
 	//".png', format = 'png', bbox_inches='tight', transparent=True)" << endl;
 	pyCode.close();
+}
+
+void Revision_0(string password, vector<unsigned> passOffset, char *buffer, unsigned long long int size)
+{
+	unsigned long long int run  = 0;
+
+	while (run < size)
+	{
+		for (unsigned i = 0; i < password.size(); i++)
+		{
+			if (run >= size)
+				break;
+
+			if ( buffer[run] != '\n')
+			{
+				buffer[run] = (char)(((int )buffer[run]- 32 + passOffset[i] )%95 + 32 );
+			}
+			// дописать, отдельная обработка для '\n'
+			run++;
+		}
+	}
+
+	/*	
+	#ifdef DEBUG
+	cout << "/----------------------------------------------------------" << endl
+		 << buffer << endl
+		 << "\\----------------------------------------------------------" << endl 
+		 << endl
+		 << endl;
+	#endif
+	*/	
 }
 
 void Revision_1 (string password, vector<unsigned> passOffset, char *buffer, unsigned long long int size)
@@ -307,12 +435,70 @@ void Revision_1 (string password, vector<unsigned> passOffset, char *buffer, uns
 
 		password.erase(password.size()-1); // сокращаем длину пароля на 1
 
-/*	#ifdef DEBUG
-	cout << "/----------------------------------------------------------" << endl
-		 << buffer << endl
-		 << "\\----------------------------------------------------------" << endl 
-		 << endl
-		 << endl;
-	#endif
-*/	}
+		/*	
+		#ifdef DEBUG
+		cout << "/----------------------------------------------------------" << endl
+			 << buffer << endl
+			 << "\\----------------------------------------------------------" << endl 
+			 << endl
+			 << endl;
+		#endif
+		*/	
+	}
+}
+
+void Revision_2 (string password, vector<char> alphabet, char *buffer, unsigned long long int size)
+{
+	unsigned rounds = password.size();
+
+	vector<unsigned> passOffset(password.size());
+
+	for ( unsigned k = 0; k < rounds; k++)
+	{
+		unsigned long long int run  = 0;
+
+		vector<unsigned> passOffset(password.size());
+
+		for (unsigned i = 0; i < password.size(); i++)
+		{
+			for (unsigned k = 0; k < alphabet.size(); k++)
+			{
+				if (password[i] == alphabet[k])
+				{
+					passOffset[i] = (unsigned)alphabet[k] - 32;
+					break;
+				}
+			}
+		}
+
+		while (run < size)
+		{
+			for (unsigned i = 0; i < password.size(); i++)
+			{
+				if (run >= size)
+					break;
+
+				if ( buffer[run] != '\n')
+				{
+					buffer[run] = (char)(((int )buffer[run]- 32 + passOffset[i] )%95 + 32 );
+				}
+				// дописать, отдельная обработка для '\n'
+				run++;
+			}
+		}
+
+		password.erase(password.size()-1); // сокращаем длину пароля на 1
+		reverse(password.begin(),password.end()); //реверс
+		//cout << password << endl;		
+
+		/*	
+		#ifdef DEBUG
+		cout << "/----------------------------------------------------------" << endl
+			 << buffer << endl
+			 << "\\----------------------------------------------------------" << endl 
+			 << endl
+			 << endl;
+		#endif
+		*/	
+	}
 }
